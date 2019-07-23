4f58c7cb6670df6b5b4d7473dbb15ad2b3005d1b
Calibrateds <53193101+Calibrateds@users.noreply.github.com>
2019-07-23 02:28:10 +0100

    added /showall

--- esx_showcommands/server.lua
+++ esx_showcommands/server.lua
@@ -72,6 +72,60 @@ TriggerClientEvent('esx:showNotification', _source, 'You currently have ~g~$~g~'
 --TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'You currently have $' .. black_money .. ' dirty money in your wallet'})
 end, {help = "Check how much dirty money you have"})
 
+TriggerEvent('es:addCommand', 'showall', function(source)
+    local _source = source
+    local xPlayer = ESX.GetPlayerFromId(_source)
+    local job = xPlayer.job.label
+    local jobgrade = xPlayer.job.grade_label
+    local wallet 		= getMoneyFromUser(_source)
+    local bank 			= getBankFromUser(_source)
+    local black_money 	= getBlackMoneyFromUser(_source)
+    if xPlayer.job.grade_name == 'boss' then
+		local society = GetSociety(xPlayer.job.name)
+
+		if society ~= nil then
+			TriggerEvent('esx_addonaccount:getSharedAccount', society.account, function(account)
+				money = account.money
+			end)
+		else
+			money = 0
+		end
+
+                TriggerClientEvent('esx:showNotification', _source, 'You are working as: ~g~' .. job .. ' ~s~-~g~ ' .. jobgrade)	         
+                --TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'You are working as: ' .. job .. ' - ' .. jobgrade})
+                Citizen.Wait(1500)
+                TriggerClientEvent('esx:showNotification', _source, 'You currently have ~g~$~g~' .. wallet .. ' ~s~in your wallet~g~ ')
+                --TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'You currently have $' .. wallet .. ' in your wallet'})
+                Citizen.Wait(1500)
+                TriggerClientEvent('esx:showNotification', _source, 'You currently have ~g~$~g~' .. bank .. ' ~s~in your bank~g~ ')
+                --TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'You currently have $' .. bank .. ' in your bank'})
+                Citizen.Wait(1500)
+                TriggerClientEvent('esx:showNotification', _source, 'You currently have ~g~$~g~' .. black_money .. ' ~s~dirty money in your wallet~g~ ')
+                --TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'You currently have $' .. black_money .. ' dirty money in your wallet'})
+                Citizen.Wait(1500)
+                TriggerClientEvent('esx:showNotification', _source, 'You currently have ~g~$~g~' .. money .. ' ~s~in the society account~g~ ')
+                --TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'You currently have $' .. money .. ' in the society account'})
+																	
+	else
+
+                TriggerClientEvent('esx:showNotification', _source, 'You are working as: ~g~' .. job .. ' ~s~-~g~ ' .. jobgrade)	         
+                --TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'You are working as: ' .. job .. ' - ' .. jobgrade})
+                Citizen.Wait(1500)
+                TriggerClientEvent('esx:showNotification', _source, 'You currently have ~g~$~g~' .. wallet .. ' ~s~in your wallet~g~ ')
+                --TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'You currently have $' .. wallet .. ' in your wallet'})
+                Citizen.Wait(1500)
+                TriggerClientEvent('esx:showNotification', _source, 'You currently have ~g~$~g~' .. bank .. ' ~s~in your bank~g~ ')
+                --TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'You currently have $' .. bank .. ' in your bank'})
+                Citizen.Wait(1500)
+                TriggerClientEvent('esx:showNotification', _source, 'You currently have ~g~$~g~' .. black_money .. ' ~s~dirty money in your wallet~g~ ')
+                --TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'You currently have $' .. black_money .. ' dirty money in your wallet'})
+                Citizen.Wait(1500)
+	        TriggerClientEvent('esx:showNotification', _source, '~r~Access not granted!')
+                --TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Access not granted!'})
+																	
+	end
+end, {help = "Check your society's balance"})
+
 TriggerEvent('es:addCommand', 'showsociety', function(source)
 	local _source = source
     local xPlayer = ESX.GetPlayerFromId(_source)
@@ -87,13 +141,13 @@ TriggerEvent('es:addCommand', 'showsociety', function(source)
 			money = 0
 		end
 		
-                TriggerClientEvent('esx:showNotification', _source, 'You currently have ~g~$~g~' .. money .. ' ~s~in the society account~g~ ')
-	        --TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'You currently have $' .. money .. ' in the society account'})
+                --TriggerClientEvent('esx:showNotification', _source, 'You currently have ~g~$~g~' .. money .. ' ~s~in the society account~g~ ')
+	        TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'You currently have $' .. money .. ' in the society account'})
 																	
 	else
 
-	        TriggerClientEvent('esx:showNotification', _source, '~r~Access not granted!')
-		--TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Access not granted!'})
+	        --TriggerClientEvent('esx:showNotification', _source, '~r~Access not granted!')
+		TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Access not granted!'})
 																			
 	end
 end, {help = "Check your society's balance"})
@@ -101,3 +155,11 @@ end, {help = "Check your society's balance"})
 TriggerEvent('esx_society:getSocieties', function(societies) 
 	RegisteredSocieties = societies
 end)
+
+function GetSociety(name)
+  for i=1, #RegisteredSocieties, 1 do
+    if RegisteredSocieties[i].name == name then
+      return RegisteredSocieties[i]
+    end
+  end
+end
